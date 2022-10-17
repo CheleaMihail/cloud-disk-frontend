17f3c1db0e9c4585f05ba9a02ae600833539f34d
MihailChlea <mikeskin10@gmail.com>
2022-10-17 16:28:44 +0300

    ferwer

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
+const { fileURLToPath } = require("url");
 
 const PORT = process.env.PORT || 8000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.static(__dirname));
